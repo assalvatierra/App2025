1cf77507dad5514168fd0ad38abb46399605fedc
Abel Salvatierra <assalvatierra@gmail.com>
2024-06-22 14:46:24 -0700

    20240622-add-mat-ui-table
    20240622-add-mat-ui-table

--- app2025.client/src/app/samples/samples.component.ts
+++ app2025.client/src/app/samples/samples.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { ActionButtonsComponent, ButtonSets } from '../common/action-buttons/action-buttons.component';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-samples',
   templateUrl: './samples.component.html',
   styleUrl: './samples.component.css',
   standalone:true,
-  imports:[ActionButtonsComponent]
+  imports:[ActionButtonsComponent,MatTableModule]
 
 })
 export class SamplesComponent {
   public buttonset = ButtonSets.forGrid;
+
+  public dataSource = [
+    {Id:0, Name:'test',Remarks:'Over'},
+    {Id:1, Name:'The',Remarks:'The Lazy'},
+    {Id:2, Name:'Quick',Remarks:'Dog'},
+    {Id:3, Name:'Brown',Remarks:'Across'},
+    {Id:4, Name:'Fox',Remarks:'The Bunk'},
+    {Id:5, Name:'Jump',Remarks:'Of the river'},
+  ];
+
 
   addClicked(event:any){
     alert("Add Event");

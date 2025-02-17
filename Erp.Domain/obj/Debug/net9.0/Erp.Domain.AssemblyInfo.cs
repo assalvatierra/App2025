07d61695cde79065f8f41c143e8a4206efdad5f8
Abel Salvatierra <assalvatierra@gmail.com>
2025-02-17 14:04:54 -0800

    setup

--- Erp.Domain/obj/Debug/net9.0/Erp.Domain.AssemblyInfo.cs
+++ Erp.Domain/obj/Debug/net9.0/Erp.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Erp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1fa3f6d292c8f3dacb843ca2c568d037599d03c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+014ecccfe5f624f402e28c2caac7fb34cc357fe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Erp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Erp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

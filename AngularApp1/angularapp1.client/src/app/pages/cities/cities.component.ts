f27d1faa90e22efd8457f0175fac102028d32e09
jahdiel <jahdielsvillosa@gmail.com>
2025-04-11 10:17:46 +0800

    update cities.ts file

---
+++ AngularApp1/angularapp1.client/src/app/pages/cities/cities.component.ts
@@ -0,0 +1,77 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { ApiService } from '../../core/api.service';
+import { EntityListTableComponent } from '../../shared/entity-list-table/entity-list-table.component';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cities',
+  standalone: false,
+  templateUrl: './cities.component.html',
+  styleUrl: './cities.component.css'
+})
+export class CitiesComponent {
+  @ViewChild('ListTable') TableList !: EntityListTableComponent;
+  public showEdit: boolean = true;
+  public dataloading: boolean = true;
+
+  constructor(private api: ApiService, private router: Router) {
+  }
+
+  ngAfterViewInit(): void {
+    this.retrieveApiData();
+  }
+
+  /* Event Handlers */
+  onAddRecord() {
+    console.log('Add record clicked');
+  }
+
+  onEdit(param: any) {
+    this.router.navigate(['/references/cities/form', param]);
+    console.log('Edit record clicked', param);
+  }
+
+  onEditDetails(param: any) {
+    console.log('Edit details clicked', param);
+  }
+
+  onArchive(param: any) {
+    console.log('Archive clicked', param);
+  }
+
+
+  /* API Calls */
+  private retrieveApiData() {
+    this.dataloading = true;
+
+    this.api.getCities()
+      .subscribe({
+        next:
+          (res: any) => {
+            this.initializeEntityList(res);
+          },
+
+        error: (err) => {
+          console.error('API Error:', err);
+        },
+
+        complete: () => {
+          console.log('API call complete');
+          this.dataloading = false;
+        }
+
+
+      });
+
+
+  }
+
+  /* Methods */
+  private initializeEntityList(param: any[]) {
+    this.TableList.initialize(param);
+  }
+
+}

b5837e51720451a75582783ded729d01bb80afa8
Abel S. Salvatierra <assalvatierra@gmail.com>
2025-05-20 01:00:12 +0800

    2025-0519--Add-authentication
    2025-0519--Add-authentication

---
+++ AngularApp1/AngularApp1.Server/Areas/Identity/Data/ErpIdentityUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace AngularApp1.Server.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ErpIdentityUser class
+public class ErpIdentityUser : IdentityUser
+{
+}
+
---
+++ AngularApp1/AngularApp1.Server/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AngularApp1.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            var token = GenerateJwtToken();
+            return Ok(new { token });
+        }
+
+        private string GenerateJwtToken()
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("123456-123456-123456-123456-123456"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "ABC",
+                audience: "ALL",
+                claims: new List<Claim>(new[] { new Claim("role", "admin") }),
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+    }
+}

738a623708cc005429569cf8982da8eeb162e79a
Abel S. Salvatierra <assalvatierra@gmail.com>
2025-09-07 17:56:34 +0000

    20250907-add-jobmain-component
    20250907-add-jobmain-component

---
+++ AngularApp1/angularapp1.client/src/app/core/services/api-job-main.service.ts
@@ -0,0 +1,83 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiJobMainService {
+
+  private url = 'http://localhost:5157';
+
+  constructor(private http: HttpClient) { }
+
+  getJobMains(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.url}/api/JobMains`).pipe(
+      map((res: any) => {
+        debugger;
+        return res.map((item: any) => ({
+          id: item.id,
+          jobDate: item.jobDate,
+          description: item.description,
+          createdOn: item.createdOn,
+          createdBy: item.createdBy,
+          lastEditOn: item.lastEditOn,
+          lastEditBy: item.lastEditBy,
+          itemStatusId: item.itemStatusId,
+          businessUnitId: item.businessUnitId
+        }));
+      })
+    );
+  }
+
+  getJobMain(id: number): Observable<any> {
+    return this.http.get<any>(`${this.url}/api/JobMains/${id}`).pipe(
+      map((item: any) => ({
+        id: item.id,
+        jobDate: item.jobDate,
+        description: item.description,
+        createdOn: item.createdOn,
+        createdBy: item.createdBy,
+        lastEditOn: item.lastEditOn,
+        lastEditBy: item.lastEditBy,
+        itemStatusId: item.itemStatusId,
+        businessUnitId: item.businessUnitId
+      }))
+    );
+  }
+
+  addJobMain(data: any): Observable<any> {
+    // Only send the specified fields
+    const payload = {
+      id: data.id,
+      jobDate: data.jobDate,
+      description: data.description,
+      createdOn: data.createdOn,
+      createdBy: data.createdBy,
+      lastEditOn: data.lastEditOn,
+      lastEditBy: data.lastEditBy,
+      itemStatusId: data.itemStatusId,
+      businessUnitId: data.businessUnitId
+    };
+    return this.http.post<any>(`${this.url}/api/JobMains`, payload);
+  }
+
+  updateJobMain(id: number, data: any): Observable<any> {
+    const payload = {
+      id: data.id,
+      jobDate: data.jobDate,
+      description: data.description,
+      createdOn: data.createdOn,
+      createdBy: data.createdBy,
+      lastEditOn: data.lastEditOn,
+      lastEditBy: data.lastEditBy,
+      itemStatusId: data.itemStatusId,
+      businessUnitId: data.businessUnitId
+    };
+    return this.http.put<any>(`${this.url}/api/JobMains/${id}`, payload);
+  }
+
+  deleteJobMain(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.url}/api/JobMains/${id}`);
+  }
+}
--- AngularApp1/angularapp1.client/src/app/pages/job-main/job-main.component.ts
+++ AngularApp1/angularapp1.client/src/app/pages/job-main/job-main.component.ts
@@ -1,11 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiJobMainService } from '../../core/services/api-job-main.service';
+import { tableField } from '../../shared/models/entityListTableField';
+
 
 @Component({
   selector: 'app-job-main',
   standalone: false,
   templateUrl: './job-main.component.html',
-  styleUrl: './job-main.component.css'
+  styleUrls: ['./job-main.component.css']
 })
 export class JobMainComponent {
+  public showEdit: boolean = true;
+  public dataloading: boolean = true;
 
+  public get tableFields() {
+    return this.getTableFields();
+  }
+
+  constructor(
+    public apiService: ApiJobMainService,
+    private router: Router
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.retrieveApiData();
+  }
+
+  onAddRecord() {
+    this.router.navigate(['job-mains/form', 2]);
+    console.log('Add record clicked');
+  }
+
+  onEdit(param: any) {
+    this.router.navigate(['job-mains/form', param]);
+    console.log('Edit record clicked', param);
+  }
+
+  onArchive(param: any) {
+    console.log('Archive clicked', param);
+  }
+
+  private retrieveApiData() {
+    this.dataloading = true;
+    this.apiService.getJobMains()
+      .subscribe({
+        next: (res: any) => {
+          this.initializeJobMainList(res);
+        },
+        error: (err) => {
+          console.error('API Error:', err);
+        },
+        complete: () => {
+          console.log('API call complete');
+          this.dataloading = false;
+        }
+      });
+  }
+
+  private initializeJobMainList(param: any[]) {
+  }
+
+  private getTableFields(): tableField[] {
+    return [
+      { key: 'id', label: 'ID' },
+      { key: 'jobDate', label: 'Job Date' },
+      { key: 'description', label: 'Description' },
+      { key: 'createdOn', label: 'Created On' },
+      { key: 'createdBy', label: 'Created By' },
+      { key: 'lastEditOn', label: 'Last Edit On' },
+      { key: 'lastEditBy', label: 'Last Edit By' },
+      { key: 'itemStatusId', label: 'Status' },
+      { key: 'businessUnitId', label: 'Business Unit' }
+    ];
+  }
 }

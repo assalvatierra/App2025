f785b8da3502f8058b59151057e97c31e55a5f96
Abel S. Salvatierra <assalvatierra@gmail.com>
2026-05-18 09:07:59 +0800

    20260518-add-datatypes for datafields

--- AngularApp1/angularapp1.client/src/app/shared/models/entityListTableField.ts
+++ AngularApp1/angularapp1.client/src/app/shared/models/entityListTableField.ts
@@ -1,5 +1,8 @@
+export type tableFieldType = 'text' | 'date' | 'currency' | 'number' | 'boolean' | 'badge' | 'progress';
+
 export interface tableField {
   key: string;
-  label: string
+  label: string;
   sortOrder?: number;
+  type?: tableFieldType; // 'text' | 'date' | 'currency' | 'number' | 'boolean' | 'badge' | 'Progress'
 }

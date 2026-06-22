f18d0cd6e37e5b2803309590a1745b1fa3baa413
Abel S. Salvatierra <assalvatierra@gmail.com>
2026-06-21 19:31:59 -0700

    Update login.component.ts

--- AngularApp1/angularapp1.client/src/app/pages/login/login.component.ts
+++ AngularApp1/angularapp1.client/src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/auth.service';
+import { ApiSysFeaturesService } from '../../core/services/api-sys-features.service';
 // import { MsalService } from '@azure/msal-angular';
 // import { AccountInfo, RedirectRequest, SilentRequest } from '@azure/msal-browser';
 // import { catchError, switchMap } from 'rxjs/operators';
@@ -15,6 +16,8 @@ import { AuthService } from '../../core/auth.service';
 export class LoginComponent  implements OnInit  {
   username = 'admin@gmail.com';
   password = 'Admin123!';
+  isEntraIdEnabled = false;
+  isBearerEnabled = false;
   // private readonly entraRedirectRequest: RedirectRequest = {
   //   scopes: ['API.Access','user.read'],
   //   redirectStartPage: '/Entities'
@@ -22,10 +25,24 @@ export class LoginComponent  implements OnInit  {
 
   constructor(
     private authService: AuthService,
+    private sysFeaturesService: ApiSysFeaturesService,
     // private msalService: MsalService,
     private router: Router) { }
 
   ngOnInit(): void {
+    // Check authentication type to determine which login method should be shown
+    this.sysFeaturesService.getAuthenticationType().subscribe({
+      next: (authType) => {
+        this.isEntraIdEnabled = authType === 'EntraID';
+        this.isBearerEnabled = authType === 'Bearer';
+      },
+      error: (error) => {
+        console.error('Error fetching authentication type:', error);
+        this.isEntraIdEnabled = false;
+        this.isBearerEnabled = false;
+      }
+    });
+
     // this.msalService.initialize()
     //   .pipe(
     //     // handleRedirectObservable uses msal-browser handleRedirectPromise under the hood
@@ -83,7 +100,6 @@ export class LoginComponent  implements OnInit  {
 
   onSubmit() {
     console.log({ username: this.username, password: this.password });
-
     this.authService.login({ username: this.username, password: this.password })
       .subscribe({
         next: (response: any) => {

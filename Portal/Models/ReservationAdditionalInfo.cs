e012fe0a90c518ced7512232a84b4c8baa2e0dc6
Abel S. Salvatierra <assalvatierra@gmail.com>
2026-07-13 17:21:37 -0700

    20260713-Additional-fields-for reservation page

--- Portal/Models/ReservationAdditionalInfo.cs
+++ Portal/Models/ReservationAdditionalInfo.cs
@@ -3,6 +3,9 @@
     public class ReservationAdditionalInfo
     {
         public string? PickupLocation { get; set; }
+        public string? PickupInfo { get; set; }
+        public string? DestinationArea { get; set; }
+        public string? DestinationInfo { get; set; }
 
         public DateTime? PickupDate { get; set; }
 
